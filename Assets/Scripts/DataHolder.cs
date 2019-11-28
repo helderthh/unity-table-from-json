4ee73b0def9d0ccfd3ae6ff996fb15d4c3b87d8c
helderth <helderthh@gmail.com>
2019-11-28 09:51:40 -0300

    Initial version

---
+++ Assets/Scripts/DataHolder.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json.Linq;
+
+
+namespace TableFromJSON
+{
+    public static class DataHolder
+    {
+        public class GenDataType : Dictionary<string, string> { }
+
+        static public string Title;
+        static public List<string> ColumnHeaders;
+        static public List<GenDataType> Data;
+
+
+        /// <summary>
+        /// Read data from fileName.
+        /// </summary>
+        /// <param name="fileName"></param>
+        static public void ReadFromFile(string fileName)
+        {
+            string filePath = Application.streamingAssetsPath + "/" + fileName;
+            if (!File.Exists(filePath))
+                return;
+
+            // read file and put its data on a JObject
+            string dataAsJson = File.ReadAllText(filePath);
+            JObject data = JObject.Parse(dataAsJson);
+
+            // read title and columns headers
+            Title = data["Title"].ToString();
+
+            ColumnHeaders = new List<string>();
+            foreach (var headerName in data["ColumnHeaders"].Children())
+                ColumnHeaders.Add(headerName.ToString());
+
+            // read data list
+            Data = new List<GenDataType>();
+            foreach (var rawData in data["Data"].Children())
+            {
+                JToken rawField;
+                var itemObj = (JObject)rawData;
+                var itemDict = new GenDataType();
+
+                // for every data item, check which fields are available
+                foreach (var headerName in ColumnHeaders)
+                {
+                    if (itemObj.TryGetValue(headerName, out rawField))
+                        itemDict[headerName] = rawField.ToString();
+                    else
+                        itemDict[headerName] = "";
+                }
+
+                Data.Add(itemDict);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TableController.cs
@@ -0,0 +1,107 @@
+﻿using TMPro;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+namespace TableFromJSON
+{
+    using GenDataType = DataHolder.GenDataType;
+
+    public class TableController : MonoBehaviour
+    {
+        public bool ShouldUseDefaultData = true;
+        public string FileName = "JsonChallenge.json";
+
+        [Header("UI")]
+        public RectTransform Table;
+        public TextMeshProUGUI Title;
+
+        [Header("Prefabs")]
+        public Transform HeaderCellPrefab;
+        public Transform CellPrefab;
+        public Transform RowPrefab;
+
+
+        void Start()
+        {
+            LoadData();
+        }
+
+        /// <summary>
+        /// Read the data from FileName and fill the table on the UI.
+        /// </summary>
+        public void LoadData()
+        {
+            Read();
+            FillUI();
+        }
+
+        void Read()
+        {
+            if (ShouldUseDefaultData)
+                UseDefaultData();
+            else
+                DataHolder.ReadFromFile(FileName);
+        }
+
+        void FillUI()
+        {
+            Clear();
+
+            // Fill title and create a row for headers
+            Title.text = DataHolder.Title;
+
+            var HeaderRow = Instantiate(RowPrefab, Table);
+
+            // fill headers
+            foreach (var headerName in DataHolder.ColumnHeaders)
+            {
+                var cell = Instantiate(HeaderCellPrefab, HeaderRow);
+                var textMeshPro = cell.GetComponent<TextMeshProUGUI>();
+                textMeshPro.text = headerName;
+            }
+
+            // create and fill the data rows
+            foreach (var item in DataHolder.Data)
+            {
+                var row = Instantiate(RowPrefab, Table);
+                foreach (var entry in item)
+                {
+                    var cell = Instantiate(CellPrefab, row);
+                    var textMeshPro = cell.GetComponent<TextMeshProUGUI>();
+                    textMeshPro.text = entry.Value;
+                }
+            }
+        }
+
+        void Clear()
+        {
+            foreach (Transform child in Table)
+                Destroy(child.gameObject);
+        }
+
+        void UseDefaultData()
+        {
+            DataHolder.Title = "Default Data";
+            DataHolder.ColumnHeaders = new List<string>() { "Name", "Age" };
+
+            DataHolder.Data = new List<GenDataType>() {
+                new GenDataType()
+                {
+                    {"Name", "Adriana"},
+                    {"Age", "31"},
+                },
+                new GenDataType()
+                {
+                    {"Name", "Helderth"},
+                    {"Age", "27"},
+                },
+                new GenDataType()
+                {
+                    {"Name", "Pablo"},
+                    {"Age", "28"},
+                },
+            };
+        }
+    }
+}
